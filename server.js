9e7b27405c6db18343437a031bcac9024469f0a9
nikhil <07nikhilraj@gmail.com>
2023-06-22 17:30:22 +0530

    updated database

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.use(express.json());
 app.use(cors());
 
 
-mongoose.connect(process.env.DATABASE, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
   .then(() => {
-    console.log('Connected to MongoDB');
+    console.log('Connected to Database');
   })
   .catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
+    console.error('Error connecting to Database:', error);
   });
 
 
